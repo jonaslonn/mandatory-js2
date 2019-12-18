c2fc77f6b0c6549186ae61cc4cfdab1ce8cba9e7
Jonas Lönn <jonas_nilsson@live.se>
2019-12-18 10:34:55 +0100

    Added controlls for max plays and disability to play on same square

--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,8 @@
 console.log("Welcome to tic tac toe");
 
 
-var count = 0;
-var max = 8;
+var count = 0; // Håller koll på vilken runda det är i spelet
+var max = 9; // Ant Max rundor
 var user;
 var userChar;
 var moves = [];
@@ -14,10 +14,14 @@ function myFunction(value) {
     
     document.getElementById(value).style.backgroundColor = "rgb(247, 149, 222)";
 
+    // Skriver ut användbar info till konsollen.
     console.log("Roundnumber:", count);
     console.log("Selected col with id", value);
 
-    // Connect char to user with modulus
+
+    //
+    // Tilldela X eller O till varannan användare med hjälp utav modulus.
+    //
     if(user === 0) 
     {     
         userChar = 'X'; 
@@ -28,16 +32,29 @@ function myFunction(value) {
         document.getElementById(value).style.color = "rgb(255, 252, 78)";
     }
 
-    // Tilldela värdet i en array med värder/ID ifrån kolumnen
-    moves[value] = userChar;
+        //
+        // Kontrollerar vald ruta/array för att inte spela på samma.
+        //
+        if(moves[value] == null)
+        {        
+        document.getElementById(value).innerHTML = userChar; // Skriver ut symbol för användaren.       
+        moves[value] = userChar; // Tilldela värdet i en array med värder/ID ifrån kolumnen
+        count++; // Plussar på räknaren för att kontrollera om spelet är slut
+        }
+        else {
+            alert("Någon har redan spelat på denna rutan"); // Om arrayn har ett värde, varna spelaren om att det är upptaget.
+        }
 
 
 
-    // Loopa arrayn
-    for(i = count; i <= max; i++)
-    {
-        console.log("ID:", moves[i], i);
-    }
+
+
+
+    // Loopa arrayn för kontrollera värden
+    // for(i = count; i <= max; i++)
+    // {
+    //     console.log("ID:", moves[i], i);
+    // }
 
 
 
@@ -76,8 +93,13 @@ function myFunction(value) {
     }
         
 
-        // Print symbol connected to user
-        document.getElementById(value).innerHTML = userChar;
 
-        count++;
+
+        // Bryter spelet vid max antal slag.
+        // Lägger annars till en runda för kontroll utav maxrundor.
+        if(count === max)
+        {
+            alert("Max antal rundor spelade");
+        }
+        
   }
